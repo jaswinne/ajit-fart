999821e67c765b7bea49f97af701f9ebc916d85a
jaswinne <jaswinne@uark.edu>
2017-12-13 18:22:07 -0600

    Switched from clouds to net neutrality

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,24 +1,36 @@
+const replacements =[
+	{
+    regex: /\bAjit Pai\b/,
+    replacement: 'A Giant Human Fart'
+  },
+	{
+		regex: /\bfarts?\b/i,
+		replacement: 'Ajit Pai'
+	}
+]
+
+
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
-	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
+
+	if (node && (node.tagName && (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea')
+	    || (Array.isArray(node.classList) && node.classList.indexOf('ace_editor') > -1))) {
 		return;
 	}
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -32,16 +44,19 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
+	var has = false;
+	replacements.forEach(function(el){
+		if(!has){
+			var vOrig = v;
+			v = v.replace(el.regex, el.replacement);
+			if(v !== vOrig){
+				has = true;
+			}
+		}
+	})
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
 	textNode.nodeValue = v;
 }
-
-
